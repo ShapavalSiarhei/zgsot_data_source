fc525e27b0f0c259287853ccaa0563b379b0cdad
Siarhei Shapaval <sa.shapoval@gmail.com>
2023-08-09 08:34:27 +0000

    GSOT Data Source

---
+++ zgsot_data_source/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zgsotdatasource.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
